7535d6387587741c5bcbd13b61e1d8d1a561dec5
sam-galileo <sam@rungalileo.io>
2023-04-06 10:56:38 -0400

    Remove table

--- components/table.tsx
+++ components/table.tsx
@@ -3,7 +3,7 @@ import { Text } from "@mantine/core";
 const Table = ({ data, columns, onSelect }) => {
   return (
     <Text color="dimmed" sx={{ fontStyle: "italic" }}>
-      [Implement Table Here]
+      
     </Text>
   );
 };
