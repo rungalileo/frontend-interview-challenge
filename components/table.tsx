67e96f2c6615852a4e91cbcbe683d5eb8586063d
sam-galileo <sam@rungalileo.io>
2023-05-10 11:32:57 -0400

    Add table

--- components/table.tsx
+++ components/table.tsx
@@ -1,10 +1,28 @@
-import { Text } from "@mantine/core";
+interface TableProps {
+  data: any[];
+  columns: string[];
+}
 
-const Table = ({ data, columns, onSelect }) => {
+const Table = ({ data, columns }: TableProps) => {
   return (
-    <Text color="dimmed" sx={{ fontStyle: "italic" }}>
-      
-    </Text>
+    <table>
+      <thead>
+        <tr>
+          {columns.map((column) => (
+            <th key={column}>{column}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row) => (
+          <tr key={row.id}>
+            {columns.map((column) => (
+              <td key={column}>{row[column]}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
