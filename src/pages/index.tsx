8e6c48ca84493386af90aea89c51e6fd23c89a44
Rodrigo Garcia <rodrigo@rungalileo.io>
2025-07-21 12:34:05 -0300

    Update deps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,22 @@
-import useGetRecipes from '@/hooks/query-hooks/use-get-recipes';
-import { Button, Card, Center, Container, ScrollArea, Stack, Title } from '@mantine/core';
+import useGetRecipes from "@/hooks/query-hooks/use-get-recipes";
+import {
+  Button,
+  Card,
+  Center,
+  Container,
+  ScrollArea,
+  Stack,
+  Title,
+} from "@mantine/core";
 
 /**
  * Instructions:
- * 
+ *
  * 1. Ensure that the home page properly renders a list of recipes.
  * 2. Add a new feature to load more recipes when the user clicks on a button.
  * 3. Add a filter feature to filter recipes.
  * 4. Add a new dynamic route to render a recipe. Users should be able to click a recipe and navigate to the recipe's page.
- * 5. The recipe's page should display the recipe's name, ingredients, and ingredients.
+ * 5. The recipe's page should display the recipe's name, ingredients, and instructions.
  */
 
 const Home = () => {
@@ -17,18 +25,18 @@ const Home = () => {
 
   // Render
   return (
-    <Container h='100vh'>
+    <Container h="100vh">
       <Title>Recipe Finder</Title>
-      <ScrollArea h={600} mt='xl'>
-        <Stack mt='xl'>
-          {recipes.map((recipe) => (
+      <ScrollArea h={600} mt="xl">
+        <Stack mt="xl">
+          {recipes?.map((recipe) => (
             <Card key={recipe.id} withBorder>
               {recipe.name}
             </Card>
           ))}
         </Stack>
       </ScrollArea>
-      <Center mt='lg'>
+      <Center mt="lg">
         <Button>Load more recipes</Button>
       </Center>
     </Container>
